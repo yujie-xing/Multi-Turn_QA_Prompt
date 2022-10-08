fba15436d448c9735bb1092a3d99ecd3e5c0384c
yujie-xing <yujie.xing@ntnu.no>
2022-10-08 10:24:07 +0300

    Added search function for start and end predictions

--- decode.py
+++ decode.py
@@ -1,15 +1,19 @@
 from GPT2forQA import *
 from data import *
 import torch
+import numpy as np
 
 
 class decode_QA():
 
-	def __init__(self, data_path, model, output_path):
+	def __init__(self, data_path, model, output_path, search_size = 10):
 
+		self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 		self.data = qa_data(data_path)
-		self.model = model
+		self.model = model.to(self.device)
 		self.output = output_path
+		self.search_size = search_size
+		
 
 
 
@@ -22,9 +26,12 @@ class decode_QA():
 			qa_dict = qa_list[0]
 			input_ids, token_type_ids = preprocess_decode(qa_dict)  ### Need to be implemented.
 			with torch.no_grad():
+				input_ids = input_ids.to(self.device)
+				token_type_ids = token_type_ids.to(self.device)
 				start_logits,end_logits = model(input_ids=input_ids,token_type_ids=token_type_ids).logits
-			predicted_start = start_logits.argmax().item()
-			predicted_end = end_logits.argmax().item()
+			start_values, start_indices = start_logits.topk(self.search_size,dim=-1)
+			end_values, end_indices = end_logits.topk(self.search_size,dim=-1)
+			predicted_start, predicted_end = self.search(start_values, start_indices, end_values, end_indices)
 			predicted_span = id_to_span(predicted_start, predicted_end) ### Need to be implemented.
 			previous_qa_dict = qa_dict
 
@@ -35,16 +42,46 @@ class decode_QA():
 				qa_dict = self.data.add_prompt_decode(qa_dict, predicted_span, previous_qa_dict)
 				input_ids, token_type_ids, prompt_positions = preprocess_decode(qa_dict)  ### Need to be implemented.
 				with torch.no_grad():
+					input_ids = input_ids.to(self.device)
+					token_type_ids = token_type_ids.to(self.device)		
 					start_logits,end_logits = model(input_ids=input_ids,token_type_ids=token_type_ids).logits
 				for prompt in prompt_positions:
 					start_logits[prompt] = float("-Inf")
 					end_logits[prompt] = float("-Inf")
-				predicted_start = start_logits.argmax(dim=-1).item()
-				predicted_end = end_logits.argmax(dim=-1).item()
+				start_values, start_indices = start_logits.topk(self.search_size,dim=-1)
+				end_values, end_indices = end_logits.topk(self.search_size,dim=-1)
+				predicted_start, predicted_end = self.search(start_values, start_indices, end_values, end_indices)
 				predicted_span = id_to_span(predicted_start, predicted_end) ### Need to be implemented.
 				previous_qa_dict = qa_dict
 
 				self.write(qa_dict, predicted_span)
+
+
+	def search(self, start_value, start_indices, end_values, end_indices):
+
+		start_values = start_values.cpu().numpy()
+		start_indices = start_indices.cpu().numpy()
+		end_values = end_value.cpu().numpy()
+		end_indices = end_indices.cpu().numpy()
+
+		predicted_start = 0
+		predicted_end = 0
+		predicted_value = 0
+
+		for i in range(start_values.shape[0]):
+			start_value = start_values[i]
+			start_indice = start_indices[i]
+			for j in range(end values.shape[0]):
+				end_value = end_values[j]
+				end_indice = end_indices[j]
+				value = start_value + end_value
+				if start_indice < end_indice and value > predicted_value:
+					predicted_start = start_indice
+					predicted_end = end_indice
+					predicted_value = value
+
+		return predicted_start, predicted_end
+
 
 
 
